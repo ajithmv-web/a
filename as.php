8fe1e5d087b330a2840512bb2bd6f889fcb348f5
ajithmv-web <ajithmv150799@gmail.com>
2021-03-18 12:23:51 +0530

    first commit

---
+++ as.php
@@ -0,0 +1,85 @@
+<?php
+if(isset($_GET["sub"])){
+$a=$_GET["txtA"];
+
+$b=10;
+$c=$a / $b. "<br>";
+
+$d=intval($c). "<br>"; 
+
+$x = $d;  
+$y = 10;
+
+echo $x * $y. "<br>";
+}
+?>
+<form method="GET">
+	<input type="txt" name="txtA"/>
+	<input type="submit" name="sub"/>
+</form>
+<?php
+if(isset($_GET["sub"])){
+$l=$_GET["txtA"];
+
+$o=10;
+$u=$l / $o. "<br>";
+
+$p=intval($u). "<br>"; 
+
+$t= $p;  
+$e =1;
+$i=10;
+$h=$t+$e. "<br>";
+echo $h*$i;
+
+}
+?>
+<form method="GET">
+	<input type="txt" name="txtA"/>
+	<input type="submit" name="sub"/>
+</form>
+<!-- $discounted_total = $total_price - ($total_price * ($discount/100)); 
+<?php
+$m=10;
+$hi=90;
+ 
+ $op=$hi-($hi*($m/100));
+ echo $op;
+ $ocp=$hi+($hi*($m/100));
+ echo $ocp;
+ ?>
+ <!-- $GST_Amount = ( $am * $per ) / 100 ;
+echo $GST_Amount+$am; -->
+<!--  <?php
+$df=90;
+$gy=10;
+$gst=($df*$gy)/100;
+echo $gst+$df;
+
+?> -->
+ -->
+
+<<!-- ?php
+for ($x = 0; $x <=100; $x++) {
+  if ($x %3!== 0) {
+    	echo" $x";
+
+  }
+  if ($x %3 == 0) {
+  	
+  	print_r(explode("\n", $x);
+  	
+  }
+  
+}
+?>
+ -->
+ <?php
+ $rank = 13;  //Sometimes empty or Null
+$hits = 0; //Sometimes empty or Null
+echo $rating = $rank/$hits;
+if(is_nan($rating)){
+  $ratings = 0;
+}
+
+?>
